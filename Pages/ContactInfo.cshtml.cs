19ca916f6c43f73f7f91c27dc71d9e776fbb7aa2
nico132a <eaa23nive@students.eaaa.dk>
2024-10-31 11:11:54 +0100

    changes

---
+++ Pages/ContactInfo.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ibas.MenuApp.Pages
+{
+    public class ContactInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
